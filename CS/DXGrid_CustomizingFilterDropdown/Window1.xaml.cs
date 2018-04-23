027eabe99e855d4ae6abf907b2e0ba127db41358
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_CustomizingFilterDropdown/Window1.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using DevExpress.Data.Filtering;
+using DevExpress.Xpf.Grid;
+
+namespace DXGrid_CustomizingFilterDropdown {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            this.DataContext = new MyViewModel();
+
+        }
+
+        private void TableView_ShowFilterPopup(object sender, FilterPopupEventArgs e) {
+            if (e.Column.FieldName != "RegistrationDate") return;
+            List<object> filterItems = new List<object>();
+            filterItems.Add(new CustomComboBoxItem()
+            {
+                DisplayValue = "(All)",
+                EditValue = new CustomComboBoxItem()
+            });
+            filterItems.Add(new CustomComboBoxItem()
+            {
+                DisplayValue = "Registered in 2008",
+                EditValue = CriteriaOperator.Parse(string.Format(
+                "[RegistrationDate] >= #{0}# AND [RegistrationDate] < #{1}#",
+                new DateTime(2008, 1, 1), new DateTime(2009, 1, 1)))
+            });
+            filterItems.Add(new CustomComboBoxItem()
+            {
+                DisplayValue = "Registered in 2009",
+                EditValue = CriteriaOperator.Parse(string.Format(
+                "[RegistrationDate] >= #{0}# AND [RegistrationDate] < #{1}#",
+                new DateTime(2009, 1, 1), new DateTime(2010, 1, 1)))
+            });
+            e.ComboBoxEdit.ItemsSource = filterItems;            
+        }
+    }
+
+    public class MyViewModel {
+        public MyViewModel() {
+            AccountList = CreateAccounts();
+        }
+
+        public List<Account> AccountList { get; set; }
+
+    
+        private List<Account> CreateAccounts() {
+            List<Account> list = new List<Account>();
+            list.Add(new Account() {
+                UserName = "Nick White",
+                RegistrationDate = DateTime.Today
+            });
+            list.Add(new Account() {
+                UserName = "Jack Rodman",
+                RegistrationDate = new DateTime(2009, 5, 10)
+            });
+            list.Add(new Account() {
+                UserName = "Sandra Sherry",
+                RegistrationDate = new DateTime(2008, 12, 22)
+            });
+            list.Add(new Account() {
+                UserName = "Sabrina Vilk",
+                RegistrationDate = DateTime.Today
+            });
+            list.Add(new Account() {
+                UserName = "Mike Pearson",
+                RegistrationDate = new DateTime(2008, 10, 18)
+            });
+            return list;
+        }
+    }
+    public class Account {
+        public string UserName { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
---
+++ VB/DXGrid_CustomizingFilterDropdown/Window1.xaml.vb
@@ -0,0 +1,53 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Documents
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Xpf.Grid
+
+Namespace DXGrid_CustomizingFilterDropdown
+
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            Me.DataContext = New MyViewModel()
+
+        End Sub
+
+        Private Sub TableView_ShowFilterPopup(ByVal sender As Object, ByVal e As FilterPopupEventArgs)
+            If e.Column.FieldName <> "RegistrationDate" Then
+                Return
+            End If
+            Dim filterItems As New List(Of Object)()
+            filterItems.Add(New CustomComboBoxItem() With {.DisplayValue = "(All)", .EditValue = New CustomComboBoxItem()})
+            filterItems.Add(New CustomComboBoxItem() With {.DisplayValue = "Registered in 2008", .EditValue = CriteriaOperator.Parse(String.Format("[RegistrationDate] >= #{0}# AND [RegistrationDate] < #{1}#", New Date(2008, 1, 1), New Date(2009, 1, 1)))})
+            filterItems.Add(New CustomComboBoxItem() With {.DisplayValue = "Registered in 2009", .EditValue = CriteriaOperator.Parse(String.Format("[RegistrationDate] >= #{0}# AND [RegistrationDate] < #{1}#", New Date(2009, 1, 1), New Date(2010, 1, 1)))})
+            e.ComboBoxEdit.ItemsSource = filterItems
+        End Sub
+    End Class
+
+    Public Class MyViewModel
+        Public Sub New()
+            AccountList = CreateAccounts()
+        End Sub
+
+        Public Property AccountList() As List(Of Account)
+
+
+        Private Function CreateAccounts() As List(Of Account)
+            Dim list As New List(Of Account)()
+            list.Add(New Account() With {.UserName = "Nick White", .RegistrationDate = Date.Today})
+            list.Add(New Account() With {.UserName = "Jack Rodman", .RegistrationDate = New Date(2009, 5, 10)})
+            list.Add(New Account() With {.UserName = "Sandra Sherry", .RegistrationDate = New Date(2008, 12, 22)})
+            list.Add(New Account() With {.UserName = "Sabrina Vilk", .RegistrationDate = Date.Today})
+            list.Add(New Account() With {.UserName = "Mike Pearson", .RegistrationDate = New Date(2008, 10, 18)})
+            Return list
+        End Function
+    End Class
+    Public Class Account
+        Public Property UserName() As String
+        Public Property RegistrationDate() As Date
+    End Class
+End Namespace
